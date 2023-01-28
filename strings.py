25c1149395a576f39692b5439692e5839d0ca9d7
Antonyuk96 <113938370+Antonyuk96@users.noreply.github.com>
2023-01-28 19:21:30 +0300

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,13 @@
+Python 3.11.1 (tags/v3.11.1:a7a450f, Dec  6 2022, 19:58:39) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> BOT_TOKEN_FILENAME = 'token.txt'
+>>> SYMBOL_X = '❌'
+>>> SYMBOL_O = '⭕'
+>>> SYMBOL_UNDEF = '◻'
+>>> ANSW_YOUR_TURN = 'Ваш ход'
+>>> ANSW_YOU_WIN = 'Вы победили'
+>>> ANSW_BOT_WIN = 'Победил бот'
+>>> ANSW_DRAW = 'Ничья'
+>>> ANSW_HELP = 'Крестики-нолики\nСоздатель: @Antonyuk96\n\n' \
+...             'Команда /new_game для начала новой игры'
+>>> ALERT_CANNOT_MOVE_TO_THIS_CELL = 'Нажимать можно только на ' + SYMBOL_UNDEF
